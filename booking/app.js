d971a478665eb9b3a1ec1309edc2ab903e69c6ee
Tanner Carrico <carricotanner@gmail.com>
2016-09-11 18:27:00 -0400

    preparing to deploy on firebase

---
+++ booking/app.js
@@ -0,0 +1,17 @@
+// create our angular module and inject firebase
+angular.module('scheduleApp', ['firebase'])
+
+// create our main controller and get access to firebase
+.controller('mainController', function($scope, $firebase) {
+  
+  // connect to firebase 
+  var ref = new Firebase("https://burning-torch-4263.firebaseio.com/days");  
+  var fb = $firebase(ref);
+
+  // sync as object 
+  var syncObject = fb.$asObject();
+
+  // three way data binding
+  syncObject.$bindTo($scope, 'days');
+  
+});
